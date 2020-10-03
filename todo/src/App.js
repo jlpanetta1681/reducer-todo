a3e3ea3bb75fc786a1a62d68584824621b8da78e
Joe-Panetta <jlpanetta1681@icloud.com>
2020-10-03 16:11:19 -0400

    branch created and save as i forgot to save my work last commit and push

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './components/TodoList';
+// import TodoListForm from './components/TodoListForm';
 
 function App() {
-  return (
-    <div className="App">
-          <p>Welcome to your project</p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>App</h1>
+			{/* <TodoListForm /> */}
+			<TodoList />
+		</div>
+	);
 }
 
-export default App;
+export default App
